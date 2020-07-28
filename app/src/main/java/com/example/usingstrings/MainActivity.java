c28a687b584fbe999bb775c8af26f9782d747733
Anuththara18 <anuththarajayakody18@gmail.com>
2020-07-28 17:07:38 +0530

    Final Commit - Using Logcat

--- app/src/main/java/com/example/usingstrings/MainActivity.java
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -8,13 +8,60 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.textViewMsg2);
         textView.setText(R.string.Msg2);
 
+        Log.i(TAG, "onCreate: ");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.i(TAG, "onPostResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    public void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
+        //Log.i( tag: "Lifecycle", msg: "onRestart() invoked" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: ");
+    }
+
 }
